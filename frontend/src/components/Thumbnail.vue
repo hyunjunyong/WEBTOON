95cc7e577812eb3f856d72e6d44c553df63c3654
Cobb-Yoo <yhj010597@naver.com>
2021-10-17 21:40:16 +0900

    썸네일 수정

--- frontend/src/components/Thumbnail.vue
+++ frontend/src/components/Thumbnail.vue
@@ -2,15 +2,19 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <v-card max-width="250">
-          <v-img max-height="150" max-width="250" src="../img/matilda.jpg" />
+        <router-link to="/webtoon_home">
+          <v-card max-width="250" v-cl v-on:click="testF">
+            <v-img max-height="150" max-width="250" src="../img/leon.jpg" />
 
-          <v-card-title>개발자의 품격</v-card-title>
+            <v-card-title md> 개발자의 품격 </v-card-title>
 
-          <v-card-text>
-            <v-row align="center" class="mx-0"> <div>식빵</div> </v-row>
-          </v-card-text>
-        </v-card>
+            <v-card-subtitle>
+              <v-row align="center" class="ml-1">
+                <div>{{ testData }}</div>
+              </v-row>
+            </v-card-subtitle>
+          </v-card>
+        </router-link>
       </v-col>
     </v-row>
   </v-container>
@@ -19,5 +23,15 @@
 <script>
 export default {
   name: "Thumbnail",
+  data() {
+    return {
+      testData: "testing",
+    };
+  },
+  methods: {
+    testF: function() {
+      this.testData = "abc";
+    },
+  },
 };
 </script>
