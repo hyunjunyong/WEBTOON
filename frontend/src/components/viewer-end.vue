12a47c1bebf7b6afffdbb51dcc06526550d7df11
hyunjunyong <hjy4649@naver.com>
2021-10-19 16:32:20 +0900

    viewer-end 컴포넌트 작성

---
+++ frontend/src/components/viewer-end.vue
@@ -0,0 +1,21 @@
+<template>
+    <Thumbnail />
+</template>
+<script>
+import Thumbnail from "./Thumbnail.vue"
+export default {
+    name:'',
+    components: {
+        Thumbnail,
+    },
+    data(){
+        return{}
+    },
+    setup(){},
+    create(){},
+    mounted(){},
+    unmounted(){},
+    methods: {}
+}
+</script>
+<style scoped></style>
