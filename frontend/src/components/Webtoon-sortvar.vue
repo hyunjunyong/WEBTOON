f2cd5e3fed3fe85ef6364985986253d547ccbb2e
hyunjunyong <hjy4649@naver.com>
2021-10-23 11:20:45 +0900

    webtoon-sortvar scroll add
    추가

--- frontend/src/components/Webtoon-sortvar.vue
+++ frontend/src/components/Webtoon-sortvar.vue
@@ -1,19 +1,46 @@
 <template>
-    <v-container fluid>
-        <v-row v-for="(toon,index) in webtoon" :key="index">
-            <v-col class="col-3"><v-img :src="toon.url" width="150" height="150" /></v-col>
-            <v-col class="col-9"><v-card>{{toon.round}} | toon.date</v-card></v-col>
-        </v-row>
-    </v-container>
-</template>
-<!-- <div class="Webtoon-sort">
-        <div v-for="(toon,index) in webtoon" :key="index" class="Webtoon-sort wid">
-            <div class="sortLeft"><img :src="toon.url" alt="" width="150" height="150"></div>
-            <div class="sortmsg">{{toon.round}} | {{toon.date}}</div>
+    <div id="app">
+  <v-app id="inspire">
+    <v-card
+      v-scroll.self="onScroll"
+      class="overflow-y-auto"
+      max-height="400"
+    >
+      <v-banner
+        class="justify-center text-h5 font-weight-light"
+        sticky
+      >
+        최신화부터 | 1화부터
+  
+        <span
+          class="font-weight-bold"
+          v-text="scrollInvoked"
+        ></span>
+      </v-banner>
+  
+      <v-card-text>
+        <div
+          :key="n"
+          class="mb-4"
+        >
+            <v-row v-for="(toon,index) in webtoon" :key="index">
+                <v-col class="col-3"><v-img :src="toon.url" width="150" height="150" /></v-col>
+                <v-col class="col-9"><v-card>{{toon.round}} | toon.date</v-card></v-col>
+            </v-row>
         </div>
-    </div> -->
-<script>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</div>    
+</template>
 
+<script>
+// <v-container fluid>
+        // <v-row v-for="(toon,index) in webtoon" :key="index">
+        //     <v-col class="col-3"><v-img :src="toon.url" width="150" height="150" /></v-col>
+        //     <v-col class="col-9"><v-card>{{toon.round}} | toon.date</v-card></v-col>
+        // </v-row>
+//     </v-container>
 export default {
     name:'Webtoon-sortvar',
     components: {},
