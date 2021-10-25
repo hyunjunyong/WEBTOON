3f35cd0953535b9b25a481b4cf21b0cd4153001e
Cobb-Yoo <yhj010597@naver.com>
2021-10-25 21:21:27 +0900

    backend 추가

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -52,32 +52,79 @@
         <span class="router-text"> | </span>
 
         <router-link to="/register_writer"
-          ><strong class="router-text">작품 등록하기</strong></router-link
+          ><strong class="router-text">{{ testingText }}</strong></router-link
         >
       </p>
 
       <v-spacer></v-spacer>
 
       <v-row class="red ma-3 serchInput">
-        <v-img
-          src="../img/free-icon-detective-magnifying-glass-14877.png"
-          max-width="30"
-          class="ma-1 radius"
-        ></v-img>
-
-        <input />
+        <v-col>
+          <v-img
+            src="../img/free-icon-detective-magnifying-glass-14877.png"
+            max-width="30"
+            class="ma-1 radius"
+          />
+        </v-col>
+        <v-col>
+          <input />
+        </v-col>
       </v-row>
 
-      <v-hover><v-avatar color="red" size="40">HJ</v-avatar></v-hover>
+      <v-avatar @click="test" color="red" size="40" v-click="test">HJ</v-avatar>
+
+      <v-card id="userStatusCard" v-if="userStatus">
+        <v-card-text class="user-state-text">
+          <router-link to="/webtoon_home">
+            작품
+          </router-link>
+        </v-card-text>
+        <v-card-text>
+          USER 2
+        </v-card-text>
+        <v-card-text>
+          USER 3
+        </v-card-text>
+        <v-card-text>
+          USER 4
+        </v-card-text>
+      </v-card>
     </v-app-bar>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: "이용약관",
+  data() {
+    return {
+      testingText: "작품등록하기",
+      userStatus: false,
+    };
+  },
+  methods: {
+    test() {
+      this.userStatus = !this.userStatus;
+    },
+  },
+};
+</script>
 
 <style>
 .router-text {
   color: black;
 }
+.user-state-text {
+  color: gray;
+  font-size: 10px;
+}
 .serchInput {
   border-radius: 5px;
+}
+#userStatusCard {
+  position: absolute;
+  top: 70px;
+  right: 20px;
+  margin: 0;
 }
 </style>
