60a3bcab0765c1acf56099d98e63ee0afcea7555
hyunjunyong <hjy4649@naver.com>
2021-10-27 18:30:16 +0900

    작가 1:1 문의 페이지
    및 에피소드이미지 미리보기 컴포넌트? 추가

---
+++ frontend/src/components/preview-episode.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 에피소드 미리보기 페이지 -->
+  <v-container>
+    <v-row align="center" justify="space-around">
+      <v-img src="../img/code.png" alt="" />
+      <v-btn>닫기</v-btn>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  setup() {},
+  create() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style scoped></style>
