a1144fce94b0e518755f9b20b7e5bfdbc5d80b5c
hyunjunyong <hjy4649@naver.com>
2021-10-27 14:16:03 +0900

    viewer 페이지 수정

--- frontend/src/views/WRITER_Home.vue
+++ frontend/src/views/WRITER_Home.vue
@@ -1,23 +1,14 @@
 <template>
   <v-container>
-    <viewerend />
-    <rejectreason />
-    <oneonone />
+    fg
   </v-container>
 </template>
 
 <script>
-import viewerend from '../components/viewer-end.vue';
-import rejectreason from '../components/reject-reason.vue'
-import oneonone from '../components/oneonone-question.vue'
 
 export default {
   name: "WRITER_Home",
   components: {
-    viewerend,
-    rejectreason,
-    oneonone,
-
   },
 };
 </script>
