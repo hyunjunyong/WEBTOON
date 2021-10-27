fe11e773c5eb76b32fdf136cf40cb927ff85c5c1
Cobb-Yoo <yhj010597@naver.com>
2021-10-27 22:18:04 +0900

    -

--- frontend/src/components/webtoon-round.vue
+++ frontend/src/components/webtoon-round.vue
@@ -2,33 +2,35 @@
   <!-- 특정 웹툰의 화수를 보여주는 컴포넌트 입니다.
     상태 관리를 통해 수정 가능합니다.
  -->
-  <div class="webtoon_round">
+  <v-container class="webtoon_round">
     <!-- 가장 기본적인 웹툰 화수를 표현하는 컴포넌트 -->
-    <v-app v-if="webtoon_round_State == 0" id="inspire">
-      <v-card v-scroll.self="onScroll" class="overflow-y-auto" max-height="400">
-        <v-banner class="justify-center text-h5 font-weight-light" sticky>
-          최신화부터 | 1화부터
-          <span class="font-weight-bold" v-text="scrollInvoked"></span>
-        </v-banner>
+    <v-card
+      v-scroll.self="onScroll"
+      class="overflow-y-auto"
+      max-height="400"
+      v-if="webtoon_round_State == 0"
+      id="inspire"
+    >
+      <v-banner class="justify-center text-h5 font-weight-light" sticky>
+        최신화부터 | 1화부터
+        <span class="font-weight-bold" v-text="scrollInvoked"></span>
+      </v-banner>
 
-        <v-card-text>
-          <div :key="n" class="mb-4">
-            <v-row v-for="(toon, index) in webtoon" :key="index">
-              <v-col cols="3"
-                ><router-link to="/viewer"
-                  ><v-img
-                    :src="toon.url"
-                    width="150"
-                    height="150"/></router-link
-              ></v-col>
-              <v-col cols="9"
-                ><v-card>{{ toon.round }} | {{ toon.date }}</v-card></v-col
-              >
-            </v-row>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-app>
+      <v-card-text>
+        <div :key="n" class="mb-4">
+          <v-row v-for="(toon, index) in webtoon" :key="index">
+            <v-col cols="3">
+              <router-link to="/viewer">
+                <v-img :src="toon.url" width="150" height="150" />
+              </router-link>
+            </v-col>
+            <v-col cols="9">
+              <v-card> {{ toon.round }} | {{ toon.date }} </v-card>
+            </v-col>
+          </v-row>
+        </div>
+      </v-card-text>
+    </v-card>
     <!--  
             작가 사용자에게 보이는 작품 홈 페이지 에피소드 컴포넌트
             
@@ -37,43 +39,42 @@
             에피소드 옆 연필 버튼 클릭시 에피소드 수정 페이지 모달 생성
             
             -->
-    <v-container fluid v-if="webtoon_round_State == 1">
-      <v-app id="inspire">
-        <v-card
-          v-scroll.self="onScroll"
-          class="overflow-y-auto"
-          max-height="400"
-        >
-          <v-banner class="justify-center font-weight-light text-end" sticky>
-            최신화부터 | 1화부터
-          </v-banner>
+    <v-card
+      v-scroll.self="onScroll"
+      class="overflow-y-auto"
+      max-height="400"
+      fluid
+      v-if="webtoon_round_State == 1"
+      id="inspire"
+    >
+      <v-banner class="justify-center font-weight-light text-end" sticky>
+        최신화부터 | 1화부터
+      </v-banner>
 
-          <router-link to="/EDIT_EPISODE" class="ma-2">
-            <v-btn block height="100">
-              <v-icon>
-                mdi-pencil
-              </v-icon>
-              Edit
-            </v-btn>
-          </router-link>
-          <v-card-text>
-            <div :key="n" class="mb-4">
-              <v-row v-for="(toon, index) in webtoon" :key="index">
-                <v-btn class="col-1" dark small color="green" height="150">
-                  <v-icon dark>mdi-pencil</v-icon></v-btn
-                >
-                <v-col class="col-3"
-                  ><v-img :src="toon.url" width="150" height="150"
-                /></v-col>
-                <v-col class="col-7"
-                  ><v-card>{{ toon.round }} | {{ toon.date }}</v-card></v-col
-                >
-              </v-row>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-app>
-    </v-container>
+      <router-link to="/EDIT_EPISODE" class="ma-2">
+        <v-btn block height="100">
+          <v-icon>
+            mdi-pencil
+          </v-icon>
+          Edit
+        </v-btn>
+      </router-link>
+      <v-card-text>
+        <div :key="n" class="mb-4">
+          <v-row v-for="(toon, index) in webtoon" :key="index">
+            <v-btn class="col-1" dark small color="green" height="150">
+              <v-icon dark>mdi-pencil</v-icon></v-btn
+            >
+            <v-col class="col-3"
+              ><v-img :src="toon.url" width="150" height="150"
+            /></v-col>
+            <v-col class="col-7"
+              ><v-card>{{ toon.round }} | {{ toon.date }}</v-card></v-col
+            >
+          </v-row>
+        </div>
+      </v-card-text>
+    </v-card>
     <!--  
         승인 페이지 에피소드 컴포넌트
         
@@ -82,7 +83,7 @@
         상태는 심사 중 / 반려 / 승인 이 있음
         
         -->
-    <v-container fluid v-if="webtoon_round_State == 2">
+    <v-card fluid v-if="webtoon_round_State == 2">
       <v-row v-for="(toon, index) in webtoon" :key="index">
         <v-col class="col-3"
           ><v-img :src="toon.url" width="150" height="150"
@@ -98,13 +99,10 @@
           </v-flex>
         </v-col>
       </v-row>
-    </v-container>
-  </div>
+    </v-card>
+  </v-container>
 </template>
-
 <script>
-import editepisode from "./edit-episode";
-
 export default {
   name: "",
   components: {},
