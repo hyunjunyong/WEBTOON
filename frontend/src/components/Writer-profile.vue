9aa42ad0abf5b69c49dd9fe3bbe36dd8597ebad9
Cobb-Yoo <yhj010597@naver.com>
2021-10-29 20:26:53 +0900

    작가 프로필 컴포넌트 수정

--- frontend/src/components/Writer-profile.vue
+++ frontend/src/components/Writer-profile.vue
@@ -1,25 +1,41 @@
 <template>
-<!-- 작가 프로필 -->
-  <v-container fluid class="justify-center">
+  <!-- 작가 프로필 -->
+  <v-container fluid>
     <v-row>
-      <v-col class="d-flex ml-10 pl-10" cols="7">
-        <v-img src="../img/writer_profile.png" alt="" height="150px" width="150px" />
-        <v-img src="../img/setting.png" alt="" height="40px" width="40px" />
-        <v-img class="align-self-end" src="../img/like.png" alt="" height="40px" width="40px" />   
+      <v-col>
+        <v-img src="../img/setting.png" height="40px" width="40px" />
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="8">
+      <v-col>
+        <div class="justify-space-between align-center">
+          <v-img :aspect-ratio="1" src="../img/writer_profile.png" />
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img
+          class="align-self-end"
+          src="../img/like.png"
+          alt=""
+          height="40px"
+          width="40px"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
         <v-card class="text-center">작가명</v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="8">
+      <v-col>
         <v-card class="text-center">작가명</v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="8">
+      <v-col>
         <v-card class="text-center">
           <p>작가의 말</p>
           <p>가입날짜</p>
@@ -27,12 +43,12 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="8">
+      <v-col>
         <v-card class="text-center">획득뱃지</v-card>
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="pt-0" cols="6">
+      <v-col class="pt-0">
         <v-img src="../img/badge.png" alt="" height="65px" width="65px" />
       </v-col>
     </v-row>
@@ -45,10 +61,8 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {},
 };
 </script>
 
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
