1c5a28009b0f590fdf19e29ebc7e19810dc068b2
Cobb-Yoo <yhj010597@naver.com>
2021-11-03 22:32:47 +0900

    헤더 검색창 위치 수정, avator창 디자인 변경

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -15,6 +15,7 @@
           <div>개발자의 품격</div>
         </div>
       </router-link>
+
       <!-- 화면 이동용 라우터 -->
       <p class="ma-4">
         <router-link to="/"
@@ -48,22 +49,78 @@
       </p>
 
       <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <!-- 검색창 -->
+      <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+      ></v-text-field>
 
-      <v-row class="red ma-3 serchInput">
-        <v-col>
-          <v-img
-            src="../img/free-icon-detective-magnifying-glass-14877.png"
-            max-width="30"
-            class="ma-1 radius"
-          />
-        </v-col>
-        <v-col>
-          <input />
-        </v-col>
-      </v-row>
+      <v-spacer></v-spacer>
 
-      <v-avatar @click="test" color="red" size="40" v-click="test">HJ</v-avatar>
+      <!-- 사용사 아바타 -->
 
+      <v-menu min-width="350px" rounded offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon x-large v-on="on" right absolute>
+            <v-avatar color="red" size="40">
+              {{ user.initials }}
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list-item-content class="justify-center">
+            <div class="mx-auto text-center">
+              <v-avatar color="red">
+                {{ user.initials }}
+              </v-avatar>
+              <h3>Team 1</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                작품
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                작가 정보 문의
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                찜한 작가
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                찜한 목록
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                작가 신청
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                기업 회원 신청
+              </v-btn>
+
+              <v-divider class="my-3"></v-divider>
+              <v-btn depressed rounded text>
+                로그아웃
+              </v-btn>
+            </div>
+          </v-list-item-content>
+        </v-card>
+      </v-menu>
+
+      <!-- 
       <v-card id="userStatusCard" v-if="userStatus">
         <v-card-text class="user-state-text">
           <router-link to="/webtoon_home">
@@ -71,24 +128,16 @@
           </router-link>
         </v-card-text>
         <v-card-text>
-          <router-link to="/작가정보문의">
-            작가 정보 문의
-          </router-link>
+          <router-link to="/작가정보문의"> </router-link>
         </v-card-text>
         <v-card-text>
-          <router-link to="/like_list_company">
-            찜한 작가
-          </router-link>
+          <router-link to="/like_list_company"> </router-link>
         </v-card-text>
         <v-card-text>
-          <router-link to="/like_list">
-            찜한 목록
-          </router-link>
+          <router-link to="/like_list"> </router-link>
         </v-card-text>
         <v-card-text>
-          <router-link to="/apply_writer">
-            작가 신청
-          </router-link>
+          <router-link to="/apply_writer"> </router-link>
         </v-card-text>
         <v-card-text>
           <router-link to="/apply_company">
@@ -96,6 +145,7 @@
           </router-link>
         </v-card-text>
       </v-card>
+      -->
     </v-app-bar>
   </v-container>
 </template>
@@ -107,6 +157,11 @@ export default {
     return {
       testingText: "작품등록하기",
       userStatus: false,
+      user: {
+        initials: "HJ",
+        fullName: "Yoo HJ",
+        email: "yhj970105@gmail.com",
+      },
     };
   },
   methods: {
