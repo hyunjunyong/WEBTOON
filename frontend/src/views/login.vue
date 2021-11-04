f3d5269dd67a15e4e1cb656dcbb71a06af272ad1
HEEJUNG <dhwjsch07@hanmail.net>
2021-11-05 02:42:10 +0900

    회원가입 페이지 추가

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,104 +1,74 @@
 <template>
   <!-- 로그인 페이지 -->
-    <v-container>
+  <v-container>
     <v-row justify="center">
-        <v-col cols="5">
-            <v-alert
-                ma="3"
-                shaped
-                prominent
-                type="error"
-                :value="NotSuccess"
+      <v-col cols="5">
+        <v-alert ma="3" shaped prominent type="error" :value="NotSuccess">
+          아이디와 비밀번호를 확인하십시오.
+        </v-alert>
+        <v-alert ma="3" shaped prominent type="success" :value="Success">
+          로그인이 완료
+        </v-alert>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>로그인</v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field v-model="email" label="이메일을 입력하세요">
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              type="password"
+              label="비밀번호를 입력하세요"
             >
-              아이디와 비밀번호를 확인하십시오.
-            </v-alert>
-            <v-alert
-                ma="3"
-                shaped
-                prominent
-                type="success"
-                :value="Success"
-            >
-              로그인이 완료
-            </v-alert>
-            <v-card>
-                <v-toolbar flat>
-                    <v-toolbar-title>로그인</v-toolbar-title>
-                </v-toolbar>
-                <div class="pa-3">
-                    <v-text-field
-                        v-model="email"
-                        label="이메일을 입력하세요"
-                    >
-                    </v-text-field>
-                    <v-text-field
-                        v-model="password"
-                        type="password"
-                        label="비밀번호를 입력하세요"
-                    >
-                </v-text-field>
-                <v-btn
-                    color="green"
-                    depressed
-                    block
-                    large
-                    @click="login()"
-                >
-                    로그인
-                </v-btn>
-                </div>
-            </v-card>
-        </v-col>
+            </v-text-field>
+            <v-btn color="green" depressed block large @click="login()">
+              로그인
+            </v-btn>
+          </div>
+        </v-card>
+      </v-col>
     </v-row>
     <v-row justify="center">
-        <v-col cols="5">
-            <v-card>
-                <router-link to="join">
-                    <v-btn
-                        color="green"
-                        depressed
-                        block
-                        large
-                    >
-                    회원가입
-                    </v-btn>
-                </router-link>
-            </v-card>
-        </v-col>
-        
+      <v-col cols="5">
+        <v-card>
+          <router-link to="join">
+            <v-btn color="green" depressed block large>
+              회원가입
+            </v-btn>
+          </router-link>
+        </v-card>
+      </v-col>
     </v-row>
-    </v-container>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "Login",
-    data() {
-        return{
-            email: null,
-            password: null,
-            allUsers: [
-              { id: 1, name: "희정", email: "heejung@gmail.com", password: "123456" },
-            ],
-            NotSuccess: false,
-            Success: false
-        }
+  name: "Login",
+  data() {
+    return {
+      email: null,
+      password: null,
+      allUsers: [
+        { id: 1, name: "희정", email: "heejung@gmail.com", password: "123456" },
+      ],
+      NotSuccess: false,
+      Success: false,
+    };
+  },
+  methods: {
+    login() {
+      let selectedUser = null;
+      this.allUsers.forEach((user) => {
+        if (user.email === this.email) selectedUser = user;
+      });
+      selectedUser === null
+        ? (this.NotSuccess = true)
+        : selectedUser.password !== this.password
+        ? (this.NotSuccess = true)
+        : (this.Success = true);
     },
-    methods: {
-        login() {
-            let selectedUser = null
-            this.allUsers.forEach(user => {
-                if(user.email === this.email) selectedUser = user                
-            })
-            selectedUser === null
-                ? (this.NotSuccess = true)
-                : selectedUser.password !== this.password
-                    ? (this.NotSuccess = true)
-                    : (this.Success = true)
-                
-            
-
-        }
-    }
-}
+  },
+};
 </script>
