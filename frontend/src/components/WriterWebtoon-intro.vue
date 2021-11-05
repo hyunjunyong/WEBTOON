123b74ca4db6032dd5cc00fd3698a488fcda771f
hyunjunyong <hjy4649@naver.com>
2021-11-05 13:52:25 +0900

    라우터 수정

--- frontend/src/components/WriterWebtoon-intro.vue
+++ frontend/src/components/WriterWebtoon-intro.vue
@@ -12,21 +12,30 @@
       <v-col cols="5">
         <v-row>
           <v-col cols="8">
-            <v-card class="mb-10" width="auto" >
+            <v-card class="mb-10" width="auto">
               눈내리는 소리
             </v-card>
           </v-col>
           <v-col cols="2">
-            <v-img contain src="../img/like.png" height="30px" width="30px" />
+            <v-btn icon color="deep-orange" @click="increment">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+            <h4>{{ count }}</h4>
           </v-col>
           <v-col cols="2">
             <router-link to="/edit_Webtoon">
-              <v-img contain src="../img/setting.png" height="30px" width="30px" />
+              <v-img
+                contain
+                src="../img/setting.png"
+                height="30px"
+                width="30px"
+              />
             </router-link>
           </v-col>
         </v-row>
         <v-card height="90px">
-          작품 소개 <br> 겨울을 빼았긴 마을에<br>눈을 찾아오려는 소년의 모험
+          작품 소개 <br />
+          겨울을 빼았긴 마을에<br />눈을 찾아오려는 소년의 모험
         </v-card>
       </v-col>
     </v-row>
@@ -39,7 +48,16 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.commit("increment");
+    },
+  },
 };
 </script>
 
