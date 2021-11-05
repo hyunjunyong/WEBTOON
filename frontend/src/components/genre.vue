443da0fc4eb19559a6e8b2e4f0e21cdd4b6df71d
Cobb-Yoo <yhj010597@naver.com>
2021-11-05 12:05:02 +0900

    -

--- frontend/src/components/genre.vue
+++ frontend/src/components/genre.vue
@@ -1,31 +1,30 @@
 <template>
-<!--  장르 선택 컴포넌트 -->
-    <v-row>
-      <v-col v-for="item in genre" :key="item.index">
-        <v-btn rounded class="text-caption">
-          {{item.name}}
-        </v-btn>
-      </v-col>
-    </v-row>
+  <!--  장르 선택 컴포넌트 -->
+  <v-row>
+    <v-col v-for="item in genre" :key="item.index">
+      <v-btn rounded class="text-caption">
+        {{ item.name }}
+      </v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: "",
-  components: {
+  components: {},
+  data() {
+    return {
+      genre: [
+        { index: "1", name: "일상" },
+        { index: "2", name: "개그" },
+        { index: "3", name: "판타지" },
+        { index: "4", name: "액션" },
+        { index: "5", name: "드라마" },
+        { index: "6", name: "로맨스" },
+        { index: "7", name: "공포/스릴러" },
+      ],
+    };
   },
-      data(){
-        return{
-            genre:[
-                    {index:"1",name:'일상'},
-                    {index:"2",name:'개그'},
-                    {index:"3",name:'판타지'},
-                    {index:"4",name:'액션'},
-                    {index:"5",name:'드라마'},
-                    {index:"6",name:'로맨스'},
-                    {index:"7",name:'공포/스릴러'},
-                ],
-        }
-    },
 };
 </script>
