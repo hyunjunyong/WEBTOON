54c67045a3802704c20f1b6a0bf504fb2bed7338
Cobb-Yoo <yhj010597@naver.com>
2021-11-20 03:42:30 +0900

    -

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,82 +3,86 @@
   맨 처음 화면으로 인기, 신작, 추천, 내가 본 웹툰을 보여준다.
   썸네일을 누르면 해당 작품페이지로 바로 이동한다.
  -->
-  <v-card>
-    <v-carousel hide-delimiters interval="6000" height="400">
-      <v-carousel-item
-        v-for="(item, i) in items"
-        :key="i"
-        :src="item.src"
-      ></v-carousel-item>
-    </v-carousel>
+  <v-card elevation="0">
+    <v-card elevation="0">
+      <v-carousel hide-delimiters interval="6000" height="400" cycle>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-card>
 
-    <v-row justify="center">
-      <v-col cols="8" class="mt-2">
-        <v-card elevation="0">
-          <v-card-title class="text-h5 font-weight-bold mb-1">
-            초절정 인기 웹툰
-          </v-card-title>
-          <v-card-subtitle class="text-h6 font-weight-bold">
-            하드코딩 3대장
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="8" class="mt-2">
+          <v-card elevation="0">
+            <v-card-title class="text-h5 font-weight-bold mb-1">
+              초절정 인기 웹툰
+            </v-card-title>
+            <v-card-subtitle class="text-h6 font-weight-bold">
+              하드코딩 3대장
+            </v-card-subtitle>
+          </v-card>
+        </v-col>
+      </v-row>
 
-    <v-row no-gutters justify="center">
-      <v-col cols="8">
-        <!-- 
+      <v-row no-gutters justify="center">
+        <v-col cols="8">
+          <!-- 
           webtoon 배열의 값을 보냄
           한줄에 4개의 썸네일을 만듬
           h는 높이
          -->
-        <TestThumbnail :webtoonState="4" :webtoon="webtoon" :h="380" />
+          <TestThumbnail :webtoonState="4" :webtoon="webtoon" :h="380" />
 
-        <!-- <Thumbnail :webtoon="n" /> -->
-      </v-col>
-    </v-row>
+          <!-- <Thumbnail :webtoon="n" /> -->
+        </v-col>
+      </v-row>
 
-    <v-row justify="center">
-      <v-col cols="8" class="mb-3">
-        <v-card elevation="0">
-          <v-card-title class="text-h5 font-weight-bold">
-            신작 웹툰
-          </v-card-title>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-row justify="center">
+        <v-col cols="8" class="mb-3">
+          <v-card elevation="0">
+            <v-card-title class="text-h5 font-weight-bold">
+              신작 웹툰
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
 
-    <v-row no-gutters justify="center">
-      <v-col cols="8">
-        <!-- 
+      <v-row no-gutters justify="center">
+        <v-col cols="8">
+          <!-- 
           webtoon 배열의 값을 보냄
           한줄에 3개의 썸네일을 만듬
           h는 높이
          -->
-        <TestThumbnail :webtoonState="3" :webtoon="webtoon" :h="330" />
-      </v-col>
-    </v-row>
+          <TestThumbnail :webtoonState="3" :webtoon="webtoon" :h="330" />
+        </v-col>
+      </v-row>
 
-    <v-row justify="center">
-      <v-col cols="8" class="mb-3">
-        <v-card elevation="0">
-          <v-card-title class="text-h5 font-weight-bold">
-            신작 웹툰
-          </v-card-title>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-row justify="center">
+        <v-col cols="8" class="mb-3">
+          <v-card elevation="0">
+            <v-card-title class="text-h5 font-weight-bold">
+              신작 웹툰
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
 
-    <v-row justify="center" class="mb-10">
-      <v-col cols="8" class="mb-5">
-        <!-- 
+      <v-row justify="center" class="mb-10">
+        <v-col cols="8" class="mb-5">
+          <!-- 
           webtoon 배열의 값을 보냄
           한줄에 2개의 썸네일을 만듬
           h는 높이
          -->
-        <TestThumbnail :webtoonState="4" :webtoon="webtoon" :h="380" />
-      </v-col>
-    </v-row>
+          <TestThumbnail :webtoonState="4" :webtoon="webtoon" :h="380" />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
@@ -94,7 +98,8 @@ export default {
     return {
       items: [
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          src:
+            "http://images.battlecomics.co.kr/web_home_banner/526/banner-bannerid_526-w_1900-h_420-t_20211112180952.jpg",
         },
         {
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
