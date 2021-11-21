3e60c4146d3e41fa8a509a85e4a7df3578d4f5c0
Cobb-Yoo <yhj010597@naver.com>
2021-11-21 15:37:14 +0900

    하드코딩 데이터 삭제

--- frontend/src/views/WEBTOON_HOMR.vue
+++ frontend/src/views/WEBTOON_HOMR.vue
@@ -62,33 +62,7 @@ export default {
   name: "WEBTOON_Home",
   data() {
     return {
-      webtoons: [
-        {
-          url: require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_005.jpg"),
-          round: "5화",
-          date: "2021-10-27",
-        },
-        {
-          url: require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_004.jpg"),
-          round: "4화",
-          date: "2021-10-20",
-        },
-        {
-          url: require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_003.jpg"),
-          round: "3화",
-          date: "2021-10-13",
-        },
-        {
-          url: require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_002.jpg"),
-          round: "2화",
-          date: "2021-10-06",
-        },
-        {
-          url: require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_001.jpg"),
-          round: "1화",
-          date: "2021-09-30",
-        },
-      ],
+      webtoons: [],
     };
   },
   components: {
