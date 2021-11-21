af67ca8fa49a9c5f18f1e001277d009907be19a0
hyunjunyong <hjy4649@naver.com>
2021-11-21 13:29:31 +0900

    에피소드뷰어 수정

--- frontend/src/components/Webtoon-image.vue
+++ frontend/src/components/Webtoon-image.vue
@@ -3,17 +3,19 @@
   웹툰에서 특정 화수를 클릭시 뷰어페이지에서 보이는 웹툰 화면
   웹툰 화면 
   -->
-  <v-row justify="center">
-    <v-col cols="10" sm="12">
-      <v-img
+  <v-container>
+    <!-- <v-img
         v-for="index in writer_Status.episodeImages"
         :key="index"
         :src="index.imageUrl"
         width="1280px"
         max-height="5120px"
-      />
-    </v-col>
-  </v-row>
+      /> 
+      일단 하드코딩으로 스타일 수정이 우선
+       max-height="5120px" 지정시 사진이 짤리는 현상
+      -->
+    <v-img v-for="index in episode" :key="index" :src="index"></v-img>
+  </v-container>
 </template>
 
 <script>
@@ -21,7 +23,15 @@ export default {
   name: "Webtoonimage",
   props: { writer_Status: Object },
   data() {
-    return {};
+    return {
+      episode: [
+        require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_001.jpg"),
+        // require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_002.jpg"),
+        // require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_003.jpg"),
+        // require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_004.jpg"),
+        // require("../img/webtoon/황녀악녀001화_JPG/사본 -황녀악녀 1화_005.jpg"),
+      ],
+    };
   },
   methods: {},
 };
--- frontend/src/views/Episode.vue
+++ frontend/src/views/Episode.vue
@@ -2,18 +2,10 @@
   <!-- 
     작품의 에피소드가 보여지는 화면
    -->
-  <v-container fluid>
-    <v-row justify="center">
-      <v-col cols="auto"><Webtoonimage /> </v-col>
+  <v-container>
+    <v-row justify="center" class="ma-0">
+      <v-col cols="8"><Webtoonimage /> </v-col>
       <!-- 웹툰 특정 화수를 클릭할 때 나타나는 이미지 컴포넌트 -->
-      <v-col>
-        <v-btn to="/" depressed elevation="5" fab large class="float-right">
-          <!-- home btn -->
-          <v-icon dark>
-            mdi-home
-          </v-icon>
-        </v-btn>
-      </v-col>
     </v-row>
 
     <v-row>
